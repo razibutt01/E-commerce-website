2b85eee481bffe8a51d9889228f0a4d308553782
razibutt01 <buttrazi84@gmail.com>
2022-08-23 23:59:35 +0500

    initial commmit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import type { AppProps } from "next/app";
+import Layout from "../Components/Layout";
+import "../styles/globals.css";
+import { StoreProvider } from "../utils/Store";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  React.useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <StoreProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </StoreProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
